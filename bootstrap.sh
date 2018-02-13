66d243d028c5b281966ec67bcf4945b7b595872d
Ty Amell <tyamell@gmail.com>
2018-02-12 20:49:28 -0800

    remove sublime, add more brew installs, update from the original dot files but customize the .macos

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-# cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")";
 
-# git pull origin master;
+git pull origin master;
 
 function doIt() {
-	rsync --verbose --exclude ".git/" --exclude ".osx" --exclude "brew.sh" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
--- brew.sh
+++ brew.sh
@@ -2,22 +2,15 @@
 
 # Install command-line tools using Homebrew.
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Make sure we’re using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade
 
-# Install GNU core utilities (those that come with OS X are outdated).
+# Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -26,59 +19,61 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
 brew install bash
-brew tap homebrew/versions
 brew install bash-completion2
-brew install caskroom/cask/brew-cask
-brew tap caskroom/homebrew-versions
 
-# Apps
-apps=(
-  evernote
-  google-chrome
-  java
-  slack
-  firefox
-  spotify
-  vagrant
-  iterm2
-  virtualbox
-  atom
-	sublime-text
-  flux
-)
-echo "installing apps..."
-brew cask install ${apps[@]}
-
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
 
-# Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
 
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
 
+# Install some tools
+brew install nmap
+brew install sqlmap
+brew install tcpflow
+brew install tcptrace
 
 # Install other useful binaries.
-brew install netpbm
-brew install nmap
-brew install ngrep
-brew install jenv
 brew install ack
-brew install bfg
-brew install dark-mode
+brew install docker
 brew install git
-brew install git-flow
 brew install git-lfs
+brew install git-flow
 brew install lua
+brew install golang
 brew install ssh-copy-id
-brew install tree
+brew install jenv
+
+# Install casks
+brew tap caskroom/versions
+brew cask install iterm2
+brew cask install docker
+brew cask install visual-studio-code
+brew cask install little-snitch
+brew cask install micro-snitch
+brew cask install google-chrome
+brew cask install gpg-suite
+brew cask install java8
+brew cask install java
 
 # Remove outdated versions from the cellar.
 brew cleanup
